f5c8c0c78932ae92021416c33b888acbdca37d7b
Michael Harris <avant.f.music@gmail.com>
2017-01-22 20:43:22 -0500

    Implimented lucky number and fortune

--- main.py
+++ main.py
@@ -15,11 +15,34 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+    fortunes = [
+    "I see much code in your future",
+    "Consider eating more fortune cookies",
+    "You have tamed the mighty Python, now you must free it onto the Spider's web!"
+    ]
+    index = random.randint(0,2)
+    return fortunes[index]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_parapgaph = "<p>" + fortune_sentence + "</p>"
+        lucky_number = random.randint(1,100)
+        number_sentence = 'Your lucky number: ' + "<strong>" +str(lucky_number) + "</strong>"
+        number_paragraph = "<p>" + number_sentence + "</p>"
+
+        cookie_again_button = "<a href='.'><button>Another cookie plz</button></a>"
+
+        content = header + fortune_parapgaph + number_paragraph + cookie_again_button
+        self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+    ("/", MainHandler)
+],debug=True)
